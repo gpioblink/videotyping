6cec91745f9455ecdc99e97e4cdc59ff64fe8128
Gpioblink <gpioblink@aol.com>
2019-02-24 15:42:36 +0900

    サンプルソースをまともなものに変更

--- scriptGenerator/main.py
+++ scriptGenerator/main.py
@@ -8,11 +8,15 @@ with closing(sqlite3.connect(dbname)) as conn:
     c = conn.cursor()
 
     query = [
-        'kick back'
+        'take',
+        'get',
+        'take care',
+        'head honcho',
+        'make'
     ]
     # TODO: よりセキュアに実装する
     select_sql = 'select data from word where entry=="{}"'.format('" OR entry== "'.join(query))
     
 
     for row in c.execute(select_sql):
-        print(json.loads(row[0]))
+        print(json.dumps(json.loads(row[0]), indent=2, ensure_ascii=False))
